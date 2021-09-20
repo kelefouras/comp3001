14789743a0789e82d54d651c149b3c979fa4bf65
Kelefouras Vasilis <kelefourasvasilis@gmail.com>
2021-09-20 13:09:21 +0100

    21-22

--- comp3001-master/GPU_3rd_Session/mmm_solutions.cu
+++ comp3001-master/GPU_3rd_Session/mmm_solutions.cu
@@ -14,16 +14,16 @@
 #include <math.h>
 #include <omp.h>
 
-#define TIMES_TO_RUN 1 //how many times the function will run
+#define TIMES_TO_RUN 10 //how many times the function will run
 
 #define N 1024 //input size - USE POWER OF 2 ONLY 
-//#define CHECK_OUTPUT   //if do not want to validate the results comment this 
+#define CHECK_OUTPUT   //if do not want to validate the results comment this 
 
-#define TILE 16 //use either 32 or 16 only
+#define TILE 32 //use either 32 or 16 only
 #define TILE_x2 TILE*2
 #define TILE_x4 TILE*4
 
-__declspec(align(64)) float C[N*N], test[N*N], A[N*N], B[N*N]; //square matrixes are considered only, stored as 1d arrays
+__declspec(align(64)) float C[N * N], test[N * N], A[N * N], B[N * N]; //square matrixes are considered only, stored as 1d arrays
 
 void MMM_init();
 void MMM_default();
@@ -31,7 +31,7 @@ int Compare_MMM();
 inline unsigned short int equal(float const a, float const b);
 
 
-#define EPSILON 0.01
+#define EPSILON 0.00001
 
 #define MAX_NUMBER_OF_BLOCKS_PER_DIM 65535 //max number of blocks that our GPU can handle (for one dimension only)
 
@@ -40,7 +40,7 @@ inline unsigned short int equal(float const a, float const b);
 
 //This implementation uses a 2d grid and 2d blocks of threads.
 //each thread computes a value in C[], thus N*N threads
-__global__ void mmm_ver1(float *C,float *A, float*B) {
+__global__ void mmm_ver1(float* C, float* A, float* B) {
 
 	/*	for (int i = 0; i < N; i++) //to be parallelized
 		  for (int j = 0; j < N; j++) //to be parallelized
@@ -81,7 +81,7 @@ __global__ void mmm_tiled(float* C, float* A, float* B) {
 
 		__syncthreads(); //all threads wait until the arrays are initialized. 
 
-		for (k = 0; k < TILE; k ++) {
+		for (k = 0; k < TILE; k++) {
 			tmp += aa[threadIdx.y][k] * bb[k][threadIdx.x]; //each thread multiplies a sub-row of A by a sub-column of B. Each thread has its own tmp register
 		}
 
@@ -101,12 +101,12 @@ __global__ void mmm_tiled_unrolled(float* C, float* A, float* B) {
 	__shared__ float aa[TILE][TILE];
 	__shared__ float bb[TILE][TILE];
 	float tmp = 0.0;
-	int k,m;
+	int k, m;
 
 	int row_A = TILE * blockIdx.y + threadIdx.y;
 	int col_B = blockIdx.x * TILE + threadIdx.x;
-	
-		
+
+
 	//initialize the shared arrays
 	for (m = 0; m < N / TILE; m++) {
 		aa[threadIdx.y][threadIdx.x] = A[N * (row_A)+(m * TILE + threadIdx.x)];
@@ -114,12 +114,12 @@ __global__ void mmm_tiled_unrolled(float* C, float* A, float* B) {
 
 		__syncthreads();
 
-		for (k = 0; k < TILE; k+=8) {
+		for (k = 0; k < TILE; k += 8) {
 			tmp += aa[threadIdx.y][k] * bb[k][threadIdx.x];
-			tmp += aa[threadIdx.y][k+1] * bb[k+1][threadIdx.x];
-			tmp += aa[threadIdx.y][k+2] * bb[k+2][threadIdx.x];
-			tmp += aa[threadIdx.y][k+3] * bb[k+3][threadIdx.x];
-			tmp += aa[threadIdx.y][k+4] * bb[k+4][threadIdx.x];
+			tmp += aa[threadIdx.y][k + 1] * bb[k + 1][threadIdx.x];
+			tmp += aa[threadIdx.y][k + 2] * bb[k + 2][threadIdx.x];
+			tmp += aa[threadIdx.y][k + 3] * bb[k + 3][threadIdx.x];
+			tmp += aa[threadIdx.y][k + 4] * bb[k + 4][threadIdx.x];
 			tmp += aa[threadIdx.y][k + 5] * bb[k + 5][threadIdx.x];
 			tmp += aa[threadIdx.y][k + 6] * bb[k + 6][threadIdx.x];
 			tmp += aa[threadIdx.y][k + 7] * bb[k + 7][threadIdx.x];
@@ -127,8 +127,8 @@ __global__ void mmm_tiled_unrolled(float* C, float* A, float* B) {
 
 		__syncthreads();
 	}
-		C[N*row_A + col_B] = tmp;
-	
+	C[N * row_A + col_B] = tmp;
+
 
 }
 
@@ -136,7 +136,7 @@ __global__ void mmm_tiled_unrolled(float* C, float* A, float* B) {
 //like mmm_tiled, with register blocking
 //This implementation uses a 2d grid and 2d blocks of threads.
 //each thread computes a value in C[], thus N*N threads
-__global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
+__global__ void mmm_tiled_regblocking_factor2(float* C, float* A, float* B) {
 
 	__shared__ float aa1[TILE][TILE];
 	__shared__ float bb1[TILE][TILE];
@@ -146,14 +146,14 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 	float tmp0 = 0.0, tmp1 = 0.0, tmp2 = 0.0, tmp3 = 0.0;
 	int k, m;
 
-	int row_A = N*(TILE_x2 * blockIdx.y + threadIdx.y);
+	int row_A = N * (TILE_x2 * blockIdx.y + threadIdx.y);
 	int col_B = blockIdx.x * TILE_x2 + threadIdx.x;
 
 
 	for (m = 0; m < N / TILE; m++) {
 		//initialize the shared arrays
 		aa1[threadIdx.y][threadIdx.x] = A[(row_A)+m * TILE + threadIdx.x];
-		aa2[threadIdx.y][threadIdx.x] = A[(row_A + N * TILE)+(m * TILE + threadIdx.x)];
+		aa2[threadIdx.y][threadIdx.x] = A[(row_A + N * TILE) + (m * TILE + threadIdx.x)];
 		bb1[threadIdx.y][threadIdx.x] = B[N * (m * TILE + threadIdx.y) + (col_B)];
 		bb2[threadIdx.y][threadIdx.x] = B[N * (m * TILE + threadIdx.y) + (col_B)+TILE];
 
@@ -171,8 +171,8 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 	}
 	C[row_A + col_B] = tmp0;
 	C[row_A + col_B + TILE] = tmp1;
-	C[row_A + N * TILE  + col_B ] = tmp2;
-	C[row_A + N * TILE + col_B  + TILE] = tmp3;
+	C[row_A + N * TILE + col_B] = tmp2;
+	C[row_A + N * TILE + col_B + TILE] = tmp3;
 
 
 }
@@ -180,7 +180,7 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 
 
 //In mmm_sw_pipeline() routine, software pipelining is used to hide memory latency. In this case, we use two  times  more  shared memory,  since  we  need  the  current tiles  and the  next ones.  When  the  current  tiles are multiplied by each other, the next tiles are loaded from DDR to shared memory, in parallel; in this way,  the  cores  do  not  remain  idle  until  the  tiles  are  fetched.  This  is  an  advanced  implementation  and perhaps out of the scope of this module.
- __global__ void mmm_sw_pipeline(float* C, float* A, float* B) {
+__global__ void mmm_sw_pipeline(float* C, float* A, float* B) {
 
 	__shared__ float aa1[32][32]; //current tile of A
 	__shared__ float bb1[32][32]; //current tile of B
@@ -195,8 +195,8 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 	const int tx = threadIdx.x;
 	const int ty = threadIdx.y;
 	int k, m;
-	const int row = by * (32 ) + ty;
-	const int col = bx * (32 ) + tx;
+	const int row = by * (32) + ty;
+	const int col = bx * (32) + tx;
 
 	float p1 = 0.0;
 
@@ -214,26 +214,26 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 		//-------------------------------------m
 		for (k = 0; k != 32; k++) {
 			p1 += aa1[ty][k] * bb1[k][tx]; //multiply the current tiles
-			
+
 		}
 
 
 		aa1_next[ty][tx] = A[row * N + (m * 32 + tx)];//load the next tiles	 
 		bb1_next[ty][tx] = B[(m * 32 + ty) * N + col];
-		
+
 
 		__syncthreads();
 
 		//----------------------------------------m+1
 		for (k = 0; k != 32; k++) {
 			p1 += aa1_next[ty][k] * bb1_next[k][tx]; //multiply the next tiles
-			
+
 		}
 
 
 		aa1[ty][tx] = A[row * N + ((m + 1) * 32 + tx)]; //load the current tiles 
 		bb1[ty][tx] = B[((m + 1) * 32 + ty) * N + col];
-		
+
 		__syncthreads();
 
 	}
@@ -242,22 +242,22 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 
 	for (int k = 0; k != 32; k++) {
 		p1 += aa1[ty][k] * bb1[k][tx];
-				
+
 	}
 	m = ((N / 32) - 1);
 	aa1_next[ty][tx] = A[row * N + (m * 32 + tx)];
 	bb1_next[ty][tx] = B[(m * 32 + ty) * N + col];
-	
+
 	__syncthreads();
 
 	for (k = 0; k != 32; k++) {
 		p1 += aa1_next[ty][k] * bb1_next[k][tx];
-		}
+	}
 
 	__syncthreads();
 
 	C[row * N + col] = p1;
-	
+
 }
 
 
@@ -266,16 +266,21 @@ __global__ void mmm_tiled_regblocking_factor2 (float* C, float* A, float* B) {
 int main()
 {
 	cudaError_t cudaStatus;
-	
+
 	//------create the cuda timers------
-		cudaEvent_t start, stop;
-		cudaEventCreate(&start);
-		cudaEventCreate(&stop);
-		float elapsed_time;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	float elapsed_time;
+
+	int devId = 0;
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, devId);
+	printf("\n Device: %s \n", prop.name);
 
 	MMM_init(); //initialize host arrays
 
-	float * C_d, *A_d, *B_d; //pointers to device arrays
+	float* C_d, * A_d, * B_d; //pointers to device arrays
 
 //---------------------------create GPU arrays------------------------------------------
 	cudaStatus = cudaMalloc((void**)&C_d, N * N * sizeof(float));//allocate memory dynamically 
@@ -318,21 +323,22 @@ int main()
 
 
 	cudaEventRecord(start, 0); //get timer value
-	
+
 	for (int it = 0; it < TIMES_TO_RUN; it++) {
 
-	//dim3 dimBlock(TILE, TILE, 1);
-	//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
-	//mmm_tiled << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
-	//mmm_sw_pipeline <<< dimGrid, dimBlock >>> (C_d, A_d, B_d);
-	
-		dim3 dimBlock(TILE, TILE, 1); 
+		//dim3 dimBlock(TILE, TILE, 1);
+		//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
+		//mmm_ver1 << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
+		//mmm_tiled << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
+		//mmm_sw_pipeline <<< dimGrid, dimBlock >>> (C_d, A_d, B_d);
+
+		dim3 dimBlock(TILE, TILE, 1);
 		dim3 dimGrid((N + TILE_x2 - 1) / (TILE_x2), (N + TILE_x2 - 1) / (TILE_x2), 1);
 		mmm_tiled_regblocking_factor2 << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
 
 	}
 
-	
+
 	cudaEventRecord(stop, 0);  //get timer value
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&elapsed_time, start, stop);
@@ -340,9 +346,9 @@ int main()
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
 
-	double flops = (double) ((double)2 * N * N * N) / (elapsed_time / TIMES_TO_RUN);
-	printf("\nGflops achieved %f ", flops/1000000);
-	
+	double flops = (double)((double)2 * N * N * N) / (elapsed_time / TIMES_TO_RUN);
+	printf("\nGflops achieved %f ", flops / 1000000);
+
 	/*  Handling function of the CUDA runtime application programming interface.
 	*   Returns the last error from a runtime call.
 	*/
@@ -362,14 +368,14 @@ int main()
 	//MMM_default();
 
 #ifdef CHECK_OUTPUT 
-	if (Compare_MMM() != 0) 
+	if (Compare_MMM() != 0)
 		printf("\n---------WRONG OUTPUT---------------\n");
-	else 
+	else
 		printf("\n---------OUTPUT IS OK---------------\n");
 #endif
-	
+
 	/* Destroy all allocations and reset all state on the current device in the current process */
-	cudaStatus=cudaDeviceReset();
+	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
 		printf("\ncuda Reset failed!");
 		return -1;
@@ -386,7 +392,7 @@ void MMM_init() {
 	//MMM
 	for (unsigned int i = 0; i < N; i++) { //printf("\n");
 		for (unsigned int j = 0; j < N; j++) {
-			C[N*i+j] = 0.0;
+			C[N * i + j] = 0.0;
 			test[N * i + j] = 0.0;
 			A[N * i + j] = (j % 9) + p; //printf(" %3.1f",A[i][j]);
 			B[N * i + j] = (j % 7) - p; //printf(" %3.1f",B[i][j]);
@@ -404,14 +410,14 @@ void MMM_default() {
 			for (int k = 0; k < N; k++)
 				C[N * i + j] += A[N * i + k] * B[N * k + j];
 
-	
+
 }
 
 
 unsigned short int equal(float const a, float const b) {
 	float temp = a - b;
 	//printf("\n %f  %f", a, b);
-	if (fabs(temp) < EPSILON)
+	if (fabs(temp/b) < EPSILON)
 		return 0; //success
 	else
 		return 1;
@@ -423,21 +429,21 @@ int Compare_MMM() {
 	float tmp;
 	int i, j, k;
 
-//optimize the following, otherwise it takes too long...however, to allow VS to use the \pragmas you must go 
-	//in project  properties and enable that (look at the lab session document for more info)
+	//optimize the following, otherwise it takes too long...however, to allow VS to use the \pragmas you must go 
+		//in project  properties and enable that (look at the lab session document for more info)
 #pragma omp parallel 
 	{
 #pragma omp for private(i, j, k, tmp)
-			for (i = 0; i < N; i++) {
-				for (j = 0; j < N; j++) {
-					tmp = 0.0;
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				tmp = 0.0;
 #pragma omp simd reduction(+:tmp) aligned(C,A,B:64)
-					for (k = 0; k < N; k++) {
-						tmp += A[N * i + k] * B[N * k + j];
-					}
-					test[N * i + j] = tmp;
+				for (k = 0; k < N; k++) {
+					tmp += A[N * i + k] * B[N * k + j];
 				}
+				test[N * i + j] = tmp;
 			}
+		}
 	}
 
 	for (i = 0; i < N; i++)
--- comp3001-master/GPU_3rd_Session/transpose_comp3003.cu
+++ comp3001-master/GPU_3rd_Session/transpose_comp3003.cu
@@ -17,10 +17,10 @@
 int compare();
 
 #define N 1024 //input size
-#define TIMES_TO_RUN 10000
+#define TIMES_TO_RUN 10000 //this is used in case the kernel runs for a very small perior of time
 #define TILE 32
 
-float A[N][N], Atranspose[N][N];
+float A[N][N], Atranspose[N][N]; //our arrays
 
 
 #define MAX_NUMBER_OF_BLOCKS_PER_DIM 65535 //max number of blocks that our GPU can handle (for one dimension only)
@@ -29,63 +29,52 @@ __device__ float device_A[N][N]; //allocate the device arrays statically (global
 __device__ float device_Atranspose[N][N]; //allocate the device arrays statically (global GPU memory)
 
 
-void initialize() {
+void initialize() {//this routine initizalies the array with some values
 
 	int i, j;
 	for (i = 0; i < N; i++)
 		for (j = 0; j < N; j++) {
-			A[i][j] = (float) (((i*3 + j) % 1000)+0.01);
+			A[i][j] = (float)(((i * 3 + j) % 1000) + 0.01);
 		}
 }
 
 
 
-
+//in this implementation, each thread copies a single element
 __global__ void transpose_ver1() {
 
 	int i = blockIdx.x * blockDim.x + threadIdx.x; //2d grid, 2d blocks
 
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-	if (i < N && j < N) { //this is not necessary
+	if (i < N && j < N) { //this is not necessary, but good practice
 		device_Atranspose[i][j] = device_A[j][i]; //each thread copies one element
 	}
 
 }
 
-__global__ void transpose_ver2() {
-
-	int i = blockIdx.x * TILE + threadIdx.x;
-
-	int j = blockIdx.y * TILE + threadIdx.y;
-
-	if (i < N && j < N) {
-		device_Atranspose[i][j] = device_A[j][i]; //each thread copies one element
-	}
-
-}
-
+//in this implementation, each thread copies multiple elements
 __global__ void transpose_ver4() {
 
 	int x = blockIdx.x * TILE + threadIdx.x;//2d grid, 2d blocks
 	int y = blockIdx.y * TILE + threadIdx.y;
 
 	for (int m = 0; m < TILE; m += 8) {
-		device_Atranspose[x][y+m] = device_A[y + m][x];//each thread copies more than one elements
+		device_Atranspose[x][y + m] = device_A[y + m][x];//each thread copies more than one elements
 	}
 
 }
 
-
+//this is like implementation_ver4 but shared memory is used
 __global__ void transpose_ver5() {
 
-	__shared__ float tile[TILE][TILE];
+	__shared__ float tile[TILE][TILE]; //try to use 'float tile[TILE][TILE+1];'
 
 	int x = blockIdx.x * TILE + threadIdx.x;//2d grid, 2d blocks
 	int y = blockIdx.y * TILE + threadIdx.y;
 
 	for (int m = 0; m < TILE; m += 8) {
-		tile[threadIdx.y+m][threadIdx.x] = device_A[y + m][x];//each thread copies more than one elements
+		tile[threadIdx.y + m][threadIdx.x] = device_A[y + m][x];//each thread copies more than one elements
 	}
 
 	__syncthreads(); //all the threads wait here until the tile array has been initialized
@@ -94,7 +83,7 @@ __global__ void transpose_ver5() {
 	y = blockIdx.x * TILE + threadIdx.y;
 
 	for (int m = 0; m < TILE; m += 8) {
-		device_Atranspose[y + m][x] = tile[threadIdx.x][threadIdx.y+m];
+		device_Atranspose[y + m][x] = tile[threadIdx.x][threadIdx.y + m];
 	}
 
 }
@@ -102,12 +91,13 @@ __global__ void transpose_ver5() {
 
 //this kernel does a normal copy (not transpose) from A to Atranspose. Atranspose it NOT the transpose of A here.
 //the purpose of this kernel is to measure the maximum performance of copying two matrices
-__global__ void normal_copy() { 
+//143 GB/sec
+__global__ void normal_copy() {
 
 	int x = blockIdx.x * TILE + threadIdx.x; //2d grid, 2d blocks
 	int y = blockIdx.y * TILE + threadIdx.y;
 
-	for (int m = 0; m < TILE; m+=8) {
+	for (int m = 0; m < TILE; m += 8) {
 		device_Atranspose[y + m][x] = device_A[y + m][x];//each thread copies more than one elements
 	}
 
@@ -127,6 +117,10 @@ int main(int argc, char* argv[])
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	float elapsed_time;
+	int devId = 0;
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, devId);
+	printf("\n Device: %s \n", prop.name);
 
 	/* Copy the A array from the HOST memory to the DEVICE memory */
 	cudaStatus = cudaMemcpyToSymbol(device_A, A, N * N * sizeof(float));
@@ -135,15 +129,14 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	
+
 	//parameters for normal_copy() 
 	//parameters for transpose_ver4()
 	//parameters for transpose_ver5()
-		dim3 dimBlock(TILE, 8, 1);
-		dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
+	dim3 dimBlock(TILE, 8, 1);
+	dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
 
 	//parameters for transpose_ver1()
-	//parameters for transpose_ver2()
 		//dim3 dimBlock(TILE, TILE, 1);
 		//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
 
@@ -151,32 +144,32 @@ int main(int argc, char* argv[])
 
 	for (int it = 0; it < TIMES_TO_RUN; it++) {
 		//normal_copy << <dimGrid, dimBlock >> > ();
-		transpose_ver4 << <dimGrid, dimBlock >> > ();
-		//transpose_ver2 << <dimGrid, dimBlock >> > ();
+		transpose_ver5 << <dimGrid, dimBlock >> > ();
+	//	transpose_ver1 << <dimGrid, dimBlock >> > ();
 	}
 
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&elapsed_time, start, stop);
 	double bandwidth = double((double)(2 * N * N * sizeof(float))) / ((elapsed_time / 1000) / TIMES_TO_RUN);
-	printf("\nElapsed time in msecs = %f - GB/sec = %f", elapsed_time, bandwidth/1000000000 );
+	printf("\nElapsed time in msecs = %f - GB/sec = %f", elapsed_time, bandwidth / 1000000000);
 
-	
-		//dim3 dimBlock(TILE, TILE, 1);
-		//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
-		//transpose_ver2 << <dimGrid, dimBlock >> > ();
-		//transpose_ver2 << <dimGrid, dimBlock >> > ();
-		//transpose_ver3 << <dimGrid, dimBlock >> > ();
 
-		
-		//transpose_ver4 << <dimGrid, dimBlock >> > ();
-	
-	
+	//dim3 dimBlock(TILE, TILE, 1);
+	//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
+	//transpose_ver2 << <dimGrid, dimBlock >> > ();
+	//transpose_ver2 << <dimGrid, dimBlock >> > ();
+	//transpose_ver3 << <dimGrid, dimBlock >> > ();
+
+
+	//transpose_ver4 << <dimGrid, dimBlock >> > ();
+
+
 
 
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
-	
+
 	/*  Handling function of the CUDA runtime application programming interface.
 	*   Returns the last error from a runtime call.
 	*/
@@ -198,7 +191,7 @@ int main(int argc, char* argv[])
 
 
 	/* Destroy all allocations and reset all state on the current device in the current process */
-	cudaStatus=cudaDeviceReset();
+	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
 		printf("\ncuda Reset failed!");
 		return -1;
@@ -212,8 +205,8 @@ int compare() {
 	int i, j;
 	for (i = 0; i < N; i++)
 		for (j = 0; j < N; j++) {
-			if ( fabs ((A[i][j]) - Atranspose[j][i]) > 0.001 ) {
-				printf("\n\wrong results at (%d, %d)\n", i, j);
+			if (fabs( (A[i][j] - Atranspose[j][i])/A[i][j]) > 0.00001) {
+				printf("\n\wrong results at (%d, %d); they are (%f, %f)\n", i, j,A[i][j],Atranspose[j][i]);
 				return -1;
 			}
 		}

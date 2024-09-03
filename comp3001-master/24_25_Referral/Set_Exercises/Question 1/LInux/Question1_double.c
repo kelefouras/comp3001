65da8d7388d479282bef2f65a2578acb000223de
Kelefouras Vasilis <kelefourasvasilis@gmail.com>
2024-09-03 13:45:09 +0100

    24_25

---
+++ comp3001-master/24_25_Referral/Set_Exercises/Question 1/LInux/Question1_double.c
@@ -0,0 +1,181 @@
+/*
+------------------DR VASILIOS KELEFOURAS-----------------------------------------------------
+------------------COMP3001 ------------------------------------------------------------------
+------------------PARALLEL PROGAMMING MODULE-------------------------------------------------
+------------------UNIVERSITY OF PLYMOUTH, SCHOOL OF ENGINEERING, COMPUTING AND MATHEMATICS---
+*/
+
+
+//compile with gcc Question1_double.c -o p -O3 -D_GNU_SOURCE  -march=native -mavx -lm -D_GNU_SOURCE -Wall -fopenmp
+
+#include <math.h>
+#include <stdio.h>
+#include <emmintrin.h>
+#include <limits.h>
+#include <pmmintrin.h>
+#include <immintrin.h>
+#include <stdint.h>	
+#include <sched.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
+#include <omp.h>
+
+
+
+
+#define N 8192 //arrays input size
+#define TIMES 1 //times to run
+
+
+//In C, all the routines must be declared
+void step1();
+void step2();
+double step3();
+void initialize();
+
+double A[N][N],X[N],Y[N] __attribute__((aligned(64)));
+
+
+int main( ) {
+
+
+double start,end;
+double reduction;
+unsigned int i;
+
+//the following code binds this thread to code number 0. Without this code, the OS will tongle the thread among the cores, to reduce heat dissipation
+cpu_set_t mask;
+CPU_ZERO(&mask);
+CPU_SET(0,&mask);
+if(sched_setaffinity(0,sizeof(mask),&mask) == -1)
+   printf("WARNING: Could not set CPU Affinity, continuing...\n");
+
+
+
+
+start=omp_get_wtime();
+
+for (i=0;i<TIMES;i++){
+
+ initialize();
+ step1();
+ step2();
+ reduction=step3();
+ 
+}
+
+end=omp_get_wtime();
+
+printf("\n output is %e \n",reduction);
+printf("\ntime elapsed is %f secs while time per run is %f secs\n",end-start, (end-start)/TIMES);
+
+
+return 0; //normally, by returning zero, we mean that the program ended successfully. 
+}
+
+
+void initialize(){
+
+unsigned int i,j;
+
+for (i=0;i<N;i++){
+X[i]=(double) (i%4)+0.01;
+Y[i]=0.0;
+}
+
+for (i=0;i<N;i++)
+for (j=0;j<N;j++){
+A[i][j]=(double) (i%3)+(j%6)-0.02;
+}
+
+
+}
+
+
+void step1(){
+
+unsigned int i;
+__m256d const1,const2,const3,const4,const5,const6,const7,const8,a,tmp;
+const1 = _mm256_set_pd(-0.94,-0.53,-0.76,-0.64);
+const2 = _mm256_set_pd(-0.95,-0.54,-0.73,-0.67);
+const3 = _mm256_set_pd(-0.96,-0.55,-0.72,-0.66);
+const4 = _mm256_set_pd(-0.97,-0.56,-0.74,-0.65);
+const5 = _mm256_set_pd(-0.98,-0.57,-0.71,-0.63);
+const6 = _mm256_set_pd(-0.99,-0.58,-0.75,-0.62);
+const7 = _mm256_set_pd(-0.98,-0.54,-0.74,-0.62);
+const8 = _mm256_set_pd(-0.94,-0.12,-0.71,-0.61);
+
+for (i=0;i<N;i+=4){
+a = _mm256_load_pd(&X[i]);
+tmp = _mm256_setzero_pd();
+tmp = _mm256_fmadd_pd(a, const1, tmp);
+tmp = _mm256_fmadd_pd(a, const2, tmp);
+tmp = _mm256_fmadd_pd(a, const3, tmp);
+tmp = _mm256_fmadd_pd(a, const4, tmp);
+tmp = _mm256_fmadd_pd(a, const5, tmp);
+tmp = _mm256_fmadd_pd(a, const6, tmp);
+tmp = _mm256_fmadd_pd(a, const7, tmp);
+tmp = _mm256_fmadd_pd(a, const8, tmp);
+_mm256_store_pd(&X[i], tmp);
+}
+
+
+}
+
+
+void step2(){
+
+unsigned int i,j;
+__m256d const1,const2,const3,const4,const5,const6,const7,const8,a,tmp;
+const1 = _mm256_set_pd(-0.94,-0.53,-0.76,-0.64);
+const2 = _mm256_set_pd(-0.95,-0.54,-0.73,-0.67);
+const3 = _mm256_set_pd(-0.96,-0.55,-0.72,-0.66);
+const4 = _mm256_set_pd(-0.97,-0.56,-0.74,-0.65);
+const5 = _mm256_set_pd(-0.98,-0.57,-0.71,-0.63);
+const6 = _mm256_set_pd(-0.99,-0.58,-0.75,-0.62);
+const7 = _mm256_set_pd(-0.98,-0.54,-0.74,-0.62);
+const8 = _mm256_set_pd(-0.94,-0.12,-0.71,-0.61);
+
+for (i=0;i<N;i++){
+ for (j=0;j<N;j+=4){
+a = _mm256_load_pd(&A[i][j]);
+tmp = _mm256_setzero_pd();
+tmp = _mm256_fmadd_pd(a, const1, tmp);
+tmp = _mm256_fmadd_pd(a, const2, tmp);
+tmp = _mm256_fmadd_pd(a, const3, tmp);
+tmp = _mm256_fmadd_pd(a, const4, tmp);
+tmp = _mm256_fmadd_pd(a, const5, tmp);
+tmp = _mm256_fmadd_pd(a, const6, tmp);
+tmp = _mm256_fmadd_pd(a, const7, tmp);
+tmp = _mm256_fmadd_pd(a, const8, tmp);
+_mm256_store_pd(&A[i][j], tmp);
+} }
+
+
+}
+
+
+double step3(){
+
+unsigned int i,j;
+double reduction=0.0;
+
+for (i=0;i<N;i++){
+ for (j=0;j<N;j++){
+  Y[i] += A[i][j] * X[j];
+} }
+
+ for (j=0;j<N;j+=8){
+  reduction+=Y[j];
+  }
+  
+  return reduction;
+  
+}
+
+
+
+
+
+
--- comp3001-master/24_25_coursework/Set_Exercises/Question 1/LInux/Question1_alternative_file.c
+++ comp3001-master/24_25_coursework/Set_Exercises/Question 1/LInux/Question1_alternative_file.c
@@ -5,9 +5,7 @@
 ------------------UNIVERSITY OF PLYMOUTH, SCHOOL OF ENGINEERING, COMPUTING AND MATHEMATICS---
 */
 
-//This is naive program that measures the maximum FLOPS achieved in a PC
-
-//compile with gcc flops.c -o p -O2 -D_GNU_SOURCE  -march=native -mavx -lm -D_GNU_SOURCE
+//compile with gcc Question1_alternative_file.c -o p -O3 -D_GNU_SOURCE  -march=native -mavx -lm -D_GNU_SOURCE -Wall -fopenmp
 
 #include <math.h>
 #include <stdio.h>
@@ -15,7 +13,7 @@
 #include <limits.h>
 #include <pmmintrin.h>
 #include <immintrin.h>
-#include <stdint.h>	/* for uint64 definition */
+#include <stdint.h>	
 #include <sched.h>
 #include <pthread.h>
 #include <sys/syscall.h>
@@ -23,29 +21,27 @@
 #include <omp.h>
 
 
-#define BILLION 1000000000L
 
-#define N 4096*16 //arrays input size
-#define TIMES 100000 //times to run
-#define TILE 20 //tile size
-#define ARITHMETICAL_OPS N*16
+
+#define N 16384 //arrays input size
+#define TIMES 1 //times to run
 
 
 //In C, all the routines must be declared
-inline void flops();
+void step1();
+void step2();
+float step3();
 void initialize();
 
-float A[N] __attribute__((aligned(64)));
+float A[N][N],X[N],Y[N] __attribute__((aligned(64)));
 
 
 int main( ) {
 
 
-struct timespec start, end; //timers
-uint64_t diff;
-double gflops;
-float out;
-int i;
+double start,end;
+float reduction;
+unsigned int i;
 
 //the following code binds this thread to code number 0. Without this code, the OS will tongle the thread among the cores, to reduce heat dissipation
 cpu_set_t mask;
@@ -55,23 +51,23 @@ if(sched_setaffinity(0,sizeof(mask),&mask) == -1)
    printf("WARNING: Could not set CPU Affinity, continuing...\n");
 
 
-initialize();
 
-/* measure monotonic time */
-clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+
+start=omp_get_wtime();
 
 for (i=0;i<TIMES;i++){
- flops();
+
+ initialize();
+ step1();
+ step2();
+ reduction=step3();
+ 
 }
 
-clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+end=omp_get_wtime();
 
-diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-gflops = (double) ARITHMETICAL_OPS / (diff / TIMES); //ARITHMETICAL_OPS /(nanoseconds/TIMES)
-printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
-printf("elapsed time = %llu mseconds\n", (long long unsigned int) diff/1000000);
-printf("output = %f \n%f GigaFLOPS achieved\n", out, gflops);
-
+printf("\n output is %e \n",reduction);
+printf("\ntime elapsed is %f secs while time per run is %f secs\n",end-start, (end-start)/TIMES);
 
 
 return 0; //normally, by returning zero, we mean that the program ended successfully. 
@@ -80,17 +76,25 @@ return 0; //normally, by returning zero, we mean that the program ended successf
 
 void initialize(){
 
-int i;
+unsigned int i,j;
+
+for (i=0;i<N;i++){
+X[i]=(float) (i%4)+0.01;
+Y[i]=0.0;
+}
 
 for (i=0;i<N;i++)
-A[i]=(float) (i%7+0.01);
+for (j=0;j<N;j++){
+A[i][j]=(float) (i%3)+(j%6)-0.02;
+}
+
 
 }
 
 
-void flops(){
+void step1(){
 
-int i;
+unsigned int i;
 __m256 const1,const2,const3,const4,const5,const6,const7,const8,a,tmp;
 const1 = _mm256_set_ps(0.91,0.21,0.24,0.84,-0.94,-0.53,-0.76,-0.64);
 const2 = _mm256_set_ps(0.92,0.22,0.25,0.85,-0.95,-0.54,-0.73,-0.67);
@@ -101,11 +105,8 @@ const6 = _mm256_set_ps(0.96,0.26,0.29,0.89,-0.99,-0.58,-0.75,-0.62);
 const7 = _mm256_set_ps(0.97,0.23,0.45,0.76,-0.98,-0.54,-0.74,-0.62);
 const8 = _mm256_set_ps(0.98,0.43,0.43,0.77,-0.94,-0.12,-0.71,-0.61);
 
-//Think of the non-vectorized version where i increases by one (i++).
-//There are 1 load, 1 store, 8 add and 8 mul operations. 
-//Arithmetic intensity=16/8bytes=2
 for (i=0;i<N;i+=8){
-a = _mm256_load_ps(&A[i]);
+a = _mm256_load_ps(&X[i]);
 tmp = _mm256_setzero_ps();
 tmp += _mm256_mul_ps(a, const1);
 tmp += _mm256_mul_ps(a, const2);
@@ -115,14 +116,62 @@ tmp += _mm256_mul_ps(a, const5);
 tmp += _mm256_mul_ps(a, const6);
 tmp += _mm256_mul_ps(a, const7);
 tmp += _mm256_mul_ps(a, const8);
-_mm256_store_ps(&A[i], tmp);
-//printf("\n%f %f %f %f %f %f %f %f\n",A[i],A[i+1],A[i+2],A[i+3],A[i+4],A[i+5],A[i+6],A[i+7]);
+_mm256_store_ps(&X[i], tmp);
 }
 
 
 }
 
 
+void step2(){
+
+unsigned int i,j;
+__m256 const1,const2,const3,const4,const5,const6,const7,const8,a,tmp;
+const1 = _mm256_set_ps(0.91,0.21,0.24,0.84,-0.94,-0.53,-0.76,-0.64);
+const2 = _mm256_set_ps(0.92,0.22,0.25,0.85,-0.95,-0.54,-0.73,-0.67);
+const3 = _mm256_set_ps(0.93,0.23,0.26,0.86,-0.96,-0.55,-0.72,-0.66);
+const4 = _mm256_set_ps(0.94,0.24,0.27,0.87,-0.97,-0.56,-0.74,-0.65);
+const5 = _mm256_set_ps(0.95,0.25,0.28,0.88,-0.98,-0.57,-0.71,-0.63);
+const6 = _mm256_set_ps(0.96,0.26,0.29,0.89,-0.99,-0.58,-0.75,-0.62);
+const7 = _mm256_set_ps(0.97,0.23,0.45,0.76,-0.98,-0.54,-0.74,-0.62);
+const8 = _mm256_set_ps(0.98,0.43,0.43,0.77,-0.94,-0.12,-0.71,-0.61);
+
+for (i=0;i<N;i++){
+ for (j=0;j<N;j+=8){
+a = _mm256_load_ps(&A[i][j]);
+tmp = _mm256_setzero_ps();
+tmp += _mm256_mul_ps(a, const1);
+tmp += _mm256_mul_ps(a, const2);
+tmp += _mm256_mul_ps(a, const3);
+tmp += _mm256_mul_ps(a, const4);
+tmp += _mm256_mul_ps(a, const5);
+tmp += _mm256_mul_ps(a, const6);
+tmp += _mm256_mul_ps(a, const7);
+tmp += _mm256_mul_ps(a, const8);
+_mm256_store_ps(&A[i][j], tmp);
+} }
+
+
+}
+
+
+float step3(){
+
+unsigned int i,j;
+float reduction=0.0;
+
+for (i=0;i<N;i++){
+ for (j=0;j<N;j++){
+  Y[i] += A[i][j] * X[j];
+} }
+
+ for (j=0;j<N;j+=8){
+  reduction+=Y[j];
+  }
+  
+  return reduction;
+  
+}
 
 
 
